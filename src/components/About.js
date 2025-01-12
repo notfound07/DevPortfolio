924c4964bbd3d0d7d1100184bfa46aa208ece9f0
Aman Bhatt <22680764+notfound07@users.noreply.github.com>
2025-01-12 15:53:26 +0530

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload, faPaintBrush, faCogs } from '@fortawesome/free-solid-svg-icons';
-import { faXTwitter, faInstagram, faGithub, faDiscord, faAndroid, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faXTwitter, faDiscord, faGithub, faInstagram, faAndroid} from '@fortawesome/free-brands-svg-icons';
 import './About.css';
 import PDF from '../assets/AmanBhattDev.pdf';
 
@@ -17,10 +17,9 @@ function About() {
                 <div className="about-section__card">
                     <h3 className="about-section__title">Who am I?</h3>
                     <span className="about-section__line"></span>
-                    <h5 className="about-section__subtitle">MERN Stack Developer Located in New Delhi</h5>
+                    <h5 className="about-section__subtitle">MERN Stack Developer | Just One Ping Away</h5>
                     <p className="about-section__description">
-                        I am a dedicated MERN Stack Developer in New Delhi, specializing in creating efficient, scalable web applications using MongoDB, Express.js, React.js, and Node.js. I build responsive, full-stack solutions tailored to your needs, ensuring seamless performance and a user-friendly experience across all platforms.
-                    </p>
+                    I am a MERN Stack Developer based in New Delhi, specializing in building responsive and scalable web applications with MongoDB, Express.js, React.js, and Node.js. As an MCA student with a BCA from GGSIPU and 6 months of freelance experience, I have a strong foundation in full-stack development, with a focus on creating user-friendly solutions. Passionate about learning, problem-solving, and teamwork, I’m eager to grow in dynamic environments and deliver impactful results. Always just one ping away!</p>
                     <button className="about-section__btn" type="button" onClick={openPdf}>
                         <FontAwesomeIcon icon={faDownload} /> Download My CV
                     </button>
@@ -71,8 +70,7 @@ function About() {
                         <div className="about-section__expertise-content">
                             <h6>UI & UX Design</h6>
                             <p className="subtitle">
-                                Using Figma, I create user-friendly UI and UX designs that make digital experiences smooth and enjoyable. My goal is to ensure that interfaces are easy to use and satisfy users across all devices. I focus on making designs both functional and visually appealing.
-                            </p>
+                            I design user-friendly interfaces using Figma to make digital experiences simple and enjoyable. I focus on creating clean, functional, and visually appealing designs that work seamlessly across all devices.                            </p>
                             <hr />
                         </div>
                     </div>
@@ -83,8 +81,7 @@ function About() {
                         <div className="about-section__expertise-content">
                             <h6>Web Development</h6>
                             <p className="subtitle">
-                                Creating dynamic, responsive websites using the MERN stack and front-end tools, ensuring smooth functionality, great performance, and a user-friendly experience tailored to your needs.
-                            </p>
+                            I build responsive, dynamic websites using the MERN stack. My expertise ensures fast performance, smooth functionality, and a user-focused experience tailored to your needs.</p>
                             <hr />
                         </div>
                     </div>
@@ -93,10 +90,9 @@ function About() {
                             <FontAwesomeIcon icon={faAndroid} className="icon-lg" />
                         </div>
                         <div className="about-section__expertise-content">
-                            <h6>Websites Deployment</h6>
+                            <h6>Website Deployment</h6>
                             <p className="subtitle">
-                                Ensuring smooth and efficient website launches with AWS infrastructure for scalable, secure, and seamless deployment from development to live production environments.
-                            </p>
+                            I handle smooth and secure website launches using tools like AWS, ensuring scalability and seamless performance. From development to live production, I make the deployment process hassle-free.</p>
                             <hr />
                         </div>
                     </div>
